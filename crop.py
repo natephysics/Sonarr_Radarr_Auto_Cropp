d7142cea04e5ee3b66f9d480b0f18f69ebfd4118
Nathan B <nate.physics@berkeley.edu>
2022-10-19 12:39:03 +0200

    Moving project to Github, mostly working for Sonar

---
+++ crop.py
@@ -0,0 +1,292 @@
+import json
+import subprocess
+import csv
+import os
+import argparse
+import pandas as pd
+from sys import exit
+from magic import Magic
+import logging
+
+# Path to CSV with manual crop data. Should contain the following columns: "Series", "Season", "Horizontal", "Vertical"
+csv_file_path = "E:\Autocrop\TVDB.csv"
+
+# Folder to store the logfiles.
+log_path = "E:\Autocrop\logs\TV"
+# Logging level (info, debug)
+logging_level = logging.INFO
+
+# Sometimes the automatic crop values are not correct for videos that don't need cropping at all.
+# By providing a tolerance we can prevent these edge cases from being cropped to odd resolutions.
+# rel_tol=10 means that the difference between the two resolutions is less than 10 pixels
+rel_tol = 18
+
+variables = [
+    "sonarr_eventtype",
+    "sonarr_isupgrade",
+    "sonarr_series_id",
+    "sonarr_series_title",
+    "sonarr_episodefile_path",
+    "sonarr_series_tvdbid",
+    "sonarr_series_tvmazeid",
+    "sonarr_series_imdbid",
+    "sonarr_series_type",
+    "sonarr_episodefile_id",
+    "sonarr_episodefile_episodecount",
+    "sonarr_episodefile_relativepath",
+    "sonarr_episodefile_path",
+    "sonarr_episodefile_episodeids",
+    "sonarr_episodefile_seasonnumber",
+    "sonarr_episodefile_episodenumbers",
+    "sonarr_episodefile_episodeairdates",
+    "sonarr_episodefile_episodeairdatesutc",
+    "sonarr_episodefile_episodetitles",
+    "sonarr_episodefile_quality",
+    "sonarr_episodefile_qualityversion",
+    "sonarr_episodefile_releasegroup",
+    "sonarr_episodefile_scenename",
+    "sonarr_episodefile_sourcepath",
+    "sonarr_episodefile_sourcefolder",
+]
+
+
+def get_logger(log_name):
+    """logger configuration helper."""
+    log_file = f"{log_path}/crop_{log_name}.log"
+    log_format = "%(asctime)s %(levelname)-10s %(message)s"
+    log_date_format = "%y/%m/%d %H:%M:%S"
+    formatter = logging.Formatter(log_format, log_date_format)
+    fhandler = logging.FileHandler(log_file)
+    fhandler.setFormatter(formatter)
+    logger = logging.getLogger(log_name)
+    if not len(logger.handlers):
+        logger.addHandler(fhandler)
+    logger.setLevel(logging_level)
+    logger.propagate = False
+    return logger
+
+
+# The error and info will be sent to different files, just to make things more simple to read and find
+error_log = get_logger("err")
+info_log = get_logger("info")
+
+
+def read_csv(csv_file):
+    with open(csv_file, "r") as file:
+        csv_dict = [
+            {k: v for k, v in row.items()}
+            for row in csv.DictReader(file, delimiter=",", skipinitialspace=True)
+        ]
+    return csv_dict
+
+
+def crop_video(source_path, destination_path, width, height):
+    # Since the FFMPEG operations is "in-place" it will overwrite the original file, which is not what we want.
+    # Since the original file is hardlinked it's safe for us to delete it first and then create a new encode with the same name.
+    # This way we can keep the original file and the new file will have the same name.
+
+    # Delete the original file
+    cmd = ["powershell", "-Command", f'del "{destination_path}"']
+    process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+    errorcode = process.returncode
+    if errorcode > 0:
+        error_log.warning(f"Failed to delete existing file: {cmd}")
+        exit(1)
+
+    # Crop the video using the source path as the input and the destination path as the output.
+    # Prepare the crop command
+    crop_cmd = [
+        "ffmpeg",
+        "-y",
+        "-i",
+        source_path,
+        "-filter:v",
+        f"crop={width}:{height}",
+        "-c:v",
+        "libx264",
+        "-preset",
+        "slow",
+        "-tune",
+        "film",
+        "-profile:v",
+        "high",
+        "-level",
+        "4.1",
+        "-crf",
+        "19",
+        "-c:a",
+        "copy",
+        "-c:s",
+        "copy",
+        "-map",
+        "0",
+        "-max_muxing_queue_size",
+        "1024",
+        "-threads",
+        "4",
+        destination_path,
+    ]
+    crop_video_process = subprocess.run(crop_cmd, shell=True)
+    errcode = crop_video_process.returncode
+    if errcode > 0:
+        error_log.warning(
+            f"cmd failed during crop_video, see above for details. Command: {crop_cmd}"
+        )
+        exit(1)
+    else:
+        info_log.info(
+            f"Video {destination_path} successfully cropped to {width}:{height}"
+        )
+
+
+def check_video_resolution(video_path, width, height):
+    # check to see the current resolution of the video.
+    cmd = f'ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of json "{video_path}"'
+    process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+    errorcode = process.returncode
+    if errorcode > 0:
+        error_log.warning(
+            "cmd failed during check_video_resolution, see above for details. Command: ",
+            cmd,
+        )
+        exit(1)
+
+    # Parse the output of the ffprobe command
+    resolution = json.loads(process.stdout)
+    current_width = resolution["streams"][0]["width"]
+    current_height = resolution["streams"][0]["height"]
+    info_log.debug(
+        f"Video {video_path} has resolution {current_width}x{current_height} and should be {width}x{height} within a tolerance of ±{rel_tol}."
+    )
+
+    # Check to see if the current resolution is within the tolerance of the desired resolution
+    if (
+        abs(int(width) - current_width) < rel_tol
+        and abs(int(height) - current_height) < rel_tol
+    ):
+        return True
+    else:
+        return False
+
+
+def get_crop_parameters(video_path):
+    # First the video_path is converted to a path that bash can understand, using the wslpath utility, which is native
+    convert_path_to_bash_cmd = f"bash -c \"wslpath -a '{video_path}'\""
+    bash_path = (
+        subprocess.run(convert_path_to_bash_cmd, shell=True, stdout=subprocess.PIPE)
+        .stdout.decode()
+        .strip("\n")
+    )
+
+    # The following command will take a 5min clip starting from 2mins into the video and then run the cropdetect filter on it, and return the most common crop parameters over that period.
+    # I'm running this in WSL because I wasn't quite able to get it to work as I'd like using windows cmd or powershell.
+    cmd = f'bash -c "ffmpeg -ss 120 -i \\"{bash_path}\\" -f matroska -t 300 -an -vf cropdetect=24:16:0 -y -crf 51 -preset ultrafast /dev/null 2>&1 | grep -o crop=.* | sort -bh | uniq -c | sort -bh | tail -n1 | grep -o crop=.* | sed \'s/.*crop=//g\'"'
+    process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+    errorcode = process.returncode
+    if errorcode > 0:
+        print(
+            "cmd failed during get_crop_parameters, see above for details. Command: ",
+            cmd,
+        )
+        exit(1)
+
+    possible_crops = process.stdout.decode().splitlines()
+    # Get most repetead resolution, that's the one with more possibilites to be right
+    crop = max(possible_crops, key=possible_crops.count)
+    width, height, _, _ = crop.split(":")
+    info_log.debug(f"Crop parameters found: {width}x{height}")
+
+    return width, height
+
+
+def sonarr_main():
+    # Import the CSV file with the list of series with manually specified crop parameters
+    show_database = pd.read_csv(csv_file_path)
+
+    # Import the relevant environment variables
+    events_vars = {variable: os.getenv(variable, False) for variable in variables}
+
+    # log the event
+    info_log.info(
+        f"New file added: {events_vars['sonarr_series_title']} S{events_vars['sonarr_episodefile_seasonnumber']}E{events_vars['sonarr_episodefile_episodenumbers']}\nSonarr_file_path => {events_vars['sonarr_episodefile_path']}\nsonarr_episodefile_sourcepath => {events_vars['sonarr_episodefile_sourcepath']}"
+    )
+    # Process the video
+    # Check to see if the source and destination file exist
+    if os.path.exists(events_vars["sonarr_episodefile_path"]) and os.path.exists(
+        events_vars["sonarr_episodefile_sourcepath"]
+    ):
+        info_log.debug(f"Video files paths are valid.")
+
+        # Check to see if the video is in the list of series with manually specified crop parameters
+
+        # select the show and season from the database
+        show = show_database[
+            (show_database["Series"] == events_vars["sonarr_series_title"])
+            & (
+                show_database["Season"]
+                == int(events_vars["sonarr_episodefile_seasonnumber"])
+            )
+        ]
+        # If it is, use the manually specified crop parameters
+        if len(show) == 1:
+            info_log.debug(
+                f"{events_vars['sonarr_series_title']} S{events_vars['sonarr_episodefile_seasonnumber']} found in datatbase with {show['Horizontal'].values[0]}x{show['Vertical'].values[0]} resolution."
+            )
+
+            # check to see if the video is already cropped
+            already_croped = check_video_resolution(
+                events_vars["sonarr_episodefile_sourcepath"],
+                show["Horizontal"].values[0],
+                show["Vertical"].values[0],
+            )
+            print(f"already_croped: {already_croped}")
+            # if the video is already cropped, skip it
+            if already_croped:
+                info_log.info("Video already in the specified resolution. Skipping...")
+
+            # else crop the video from the source to the destination
+            else:
+                info_log.info("Begining crop video!")
+                crop_video(
+                    events_vars["sonarr_episodefile_sourcepath"],
+                    events_vars["sonarr_episodefile_path"],
+                    show["Horizontal"].values[0],
+                    show["Vertical"].values[0],
+                )
+
+        # If the video isn't in the show_database get the crop parameters automatically
+        else:
+            info_log.debug(
+                f"Video files {events_vars['sonarr_series_title']} S{events_vars['sonarr_episodefile_seasonnumber']}E{events_vars['sonarr_episodefile_episodenumbers']} not found in TVDB. Getting crop parameters automatically."
+            )
+            width, height = get_crop_parameters(
+                events_vars["sonarr_episodefile_sourcepath"]
+            )
+
+            # check to see if the video is already cropped
+            already_croped = check_video_resolution(
+                events_vars["sonarr_episodefile_sourcepath"], width, height,
+            )
+
+            # Crop the video if it isn't already cropped
+            if not already_croped:
+                crop_video(
+                    events_vars["sonarr_episodefile_sourcepath"],
+                    events_vars["sonarr_episodefile_path"],
+                    width,
+                    height,
+                )
+    else:
+        error_log.warning(
+            f"Unable to find specified file(s). Sonarr_file_path => {events_vars['sonarr_episodefile_path']}\nsonarr_episodefile_sourcepath => {events_vars['sonarr_episodefile_sourcepath']}"
+        )
+        exit(1)
+
+
+if __name__ == "__main__":
+    EventType = os.getenv("sonarr_eventtype")
+    if EventType == "Test":
+        info_log.info("Test has been ran successfully.")
+        exit(0)
+    sonarr_main()
+    info_log.info("Completed script!")
